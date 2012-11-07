318bfbd8b87406dedd3dfc3664730d0087cbb86b
Emilio Marical <emilio.mariscal@voolks.com>
2012-11-07 09:54:08 -0500

    Updated pip requirements

--- app/settings.py
+++ app/settings.py
@@ -79,7 +79,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/var/www/stq/dev.sqlite',                      # Or path to database file if using sqlite3.
+        'NAME': '/var/www/sotsiaal/dev.sqlite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
